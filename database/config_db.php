7194492adccb982efb062147063dba689fd0ac3f
FigueroaShalom <isaishalomfigueroahuerta@gmail.com>
2026-05-17 16:50:11 -0600

    Corrige todas las credenciales MySQL de IONOS

--- database/config_db.php
+++ database/config_db.php
@@ -3,6 +3,6 @@
 
 $host     = "db5020481855.hosting-data.io";
 $usuario  = "dbu928705";
-$password = "jFUHvV73f68#HPF";
-$base     = "dbs15685592";
+$password = "Hydron2025!";
+$base     = "life_below_blog";
 ?>
